3e589a1830f779a92517225a1ede20521228413c
Prayush Adhikari <115662438+adhikareeprayush@users.noreply.github.com>
2023-12-11 17:26:22 +0545

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,16 +1,16 @@
 <?php
-    //database connection
-    $hname = "localhost";
-    $uname = "root";
-    $password = "";
+    // Database connection parameters
+    $hname = "localhost"; // Hostname or IP address of the database server
+    $uname = "root";      // Database username
+    $password = "";       // Database password
 
-    $db_name = "php_projects";
+    $db_name = "php_projects"; // Name of the database
 
+    // Establishing a connection to the database
     $conn = mysqli_connect($hname, $uname, $password, $db_name);
 
+    // Check if the connection was successful
     if(!$conn){
-        echo "Connection Failed";
+        echo "Connection Failed"; // Display an error message if the connection fails
     }
-
-
 ?>
